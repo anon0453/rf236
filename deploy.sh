05a62494d62b24f12532a6d86250c7a11294483d
anon0453 <102522642+anon0453@users.noreply.github.com>
2022-03-28 12:15:52 +0100

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install Docker 
+sudo apt-get install docker-compose
+mkdir -p webbase/websites
+mkdir -p webbase/mysqldata
+mkdir conf2
+touch webbase/websites/index.html
+echo '<html><body><h1> This is a test page</h1></body></html>' > webbase/websites/index.html
+touch webbase/websites/index.htm
+echo '<html><body><h1> This is a test page</h1></body></html>' > webbase/websites/index.htm
+wget -O webbase/sqldat.sql https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/SQL/sqldat.sql
+wget -O docker-compose.yaml https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/docker-compose.yaml
+wget -O default.conf.template https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/default.conf.template
+wget -O webbase/websites/index.php https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/PHP_App/index.php
+wget -O phpDockerfile https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/PHP_App/phpDockerfile
+wget -O Dockerfile https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/NGINX/Dockerfile
+wget -O nginx.conf https://raw.githubusercontent.com/MichaelHarwood/COMP3004/main/NGINX/nginx.conf
+sudo docker-compose up -d
